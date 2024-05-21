1a029eac1a500934d783851e9578117c8a8aba09
user-lees <162332059+user-lees@users.noreply.github.com>
2024-05-21 19:17:11 +0900

    Add files via upload

---
+++ 0521/scanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int numinput;
+    char charinput;
+    scanf("%d", &numinput);
+    printf("%d", numinput);
+    scanf(" %c",&charinput);
+    printf("%c", charinput);
+    return 0;
+}
